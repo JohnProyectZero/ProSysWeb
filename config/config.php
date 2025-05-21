35037bd7dfe8ca09349c3d94b90ebaf91916be5c
JohnProyectZero <jhonatan.najarro@outlook.es>
2025-05-20 20:32:13 -0500

    -

--- config/config.php
+++
@@ -1,6 +0,0 @@
-<?php	
-    //Crear las constantes CONTROLADOR_PRINCIPAL y ACCION_PRINCIPAL para guardar el nombre de los archivos Vehiculos e index
-    define("CONTROLADOR_PRINCIPAL", "Vehiculos");
-    //define("CONTROLADOR_PRINCIPAL", "Vehiculos");
-    define("ACCION_PRINCIPAL", "index");
-
